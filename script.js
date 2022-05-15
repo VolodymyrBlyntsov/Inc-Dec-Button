2463193a11947e8562c2175c27a52446db9b1b7e
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-05-15 11:59:07 +0300

    Increment & Decrement Button
    Implemented increment button in addition to increment. Slightly improved design.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const plus = document.querySelector(".plus");
+const minus = document.querySelector('.minus');
 const num = document.querySelector(".num");
 
 let value = 1;
@@ -8,3 +9,15 @@ plus.addEventListener("click", ()=>{
 	value = (value < 10) ? "0" + value : value;
 	num.innerText = value;
 });
+
+minus.addEventListener("click", ()=>{
+	if (value > 1) {
+		value--;
+		value = (value < 10) ? "0" + value : value;
+		num.innerText = value;
+	}
+});
+
+plus.onmousedown = plus.onselectstart = () => { return false };
+
+minus.onmousedown = minus.onselectstart = ()=> { return false };
